49756cb1f6d0239ce1fe1ce9411ce4462029975c
Jacquiline Genato <jacquiline.genato@gmail.com>
2022-11-19 16:02:00 +0800

    add image and date

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CoverImage { get; set; }
     }
 }
